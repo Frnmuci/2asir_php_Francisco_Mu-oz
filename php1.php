876c30505aafb2e5ae0e287faa04438db4cfe61a
Francisco Muñoz Ciifuentes <franmuci@hotmail.com>
2024-01-22 11:32:39 +0100

    asa

--- php1.php
+++ php1.php
@@ -1,1 +1,12 @@
-prueba visual studio code
+<?php
+
+//Dado un número o una variable alterar el contenido de esa variable por el opuesto.
+
+$num = 5;
+echo $num;
+echo "<br>"; 
+function inverso(&$num) {
+    $num = -1 * $num;
+}
+inverso($num);
+echo $num;
